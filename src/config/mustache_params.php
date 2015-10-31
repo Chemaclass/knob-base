3c8456c3b5bbee128f3df8110f3396501688a806
Chemaclass <chemaclass@outlook.es>
2015-10-31 21:38:54 +0100

    Rename MustacheParams to mustache_params file

--- src/config/mustache_params.php
+++ src/config/mustache_params.php
@@ -1,13 +1,12 @@
 <?php
 /*
- * This file is part of the Knob-base package.
- *
- * (c) José María Valera Reales <chemaclass@outlook.es>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-namespace Knob\Config;
+* This file is part of the Knob-base package.
+*
+* (c) José María Valera Reales <chemaclass@outlook.es>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 
 use Knob\I18n\I18n;
 use Knob\Models\Post;
@@ -15,8 +14,8 @@ use Knob\Libs\Env;
 use Knob\Models\User;
 
 /**
- * Params to Mustache templates.
- */
+* Params to Mustache templates.
+*/
 return [
 
     'adminEmail' => ADMIN_EMAIL,
