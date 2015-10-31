eb2bf721a9ff8a468a49257e90b8677c6977108a
Chemaclass <chemaclass@outlook.es>
2015-10-31 21:42:13 +0100

    Rename and improve libs/Temaplate to libs/MustacheRender

--- src/libs/MustacheRender.php
+++ src/libs/MustacheRender.php
@@ -10,7 +10,6 @@
 namespace Knob\Libs;
 
 use Knob\I18n\I18n;
-use Knob\Config\Params;
 use Mustache_Engine;
 use Mustache_Loader_FilesystemLoader;
 use Mustache_Logger_StreamLogger;
@@ -20,10 +19,12 @@ use Mustache_Logger_StreamLogger;
  *
  * @author José María Valera Reales
  */
-class Template
+class MustacheRender
 {
 
     static $mustacheHelpersFile = 'mustache_helpers';
+
+    static $mustacheParamsFile = 'mustache_params';
 
     static $templatesDir = 'templates';
 
@@ -41,39 +42,9 @@ class Template
     /*
      * Singleton
      */
-    private static $instance = null;
+    protected static $mustacheRender = null;
 
-    /*
-     * Members
-     */
-    protected $renderEngine = null;
-
-    /**
-     * Constructor
-     */
-    private function __construct()
-    {
-        /*
-         * Render Engine.
-         */
-        $templatesFolder = static::getTemplatesFolderLocation();
-        $this->renderEngine = new Mustache_Engine(
-            [
-                'charset' => static::CHARSET,
-                'strict_callables' => static::STRICT_CALLABLES,
-                'cache_file_mode' => static::CACHE_FILE_MODE,
-                'cache_lambda_templates' => static::CACHE_LAMBDA_TEMPLATES,
-                'loader' => new Mustache_Loader_FilesystemLoader($templatesFolder),
-                'partials_loader' => new Mustache_Loader_FilesystemLoader($templatesFolder),
-                'logger' => new Mustache_Logger_StreamLogger('php://stderr'),
-                'helpers' => self::getHelpers(),
-                'pragmas' => self::getPragmas(),
-                'escape' => function ($value)
-                {
-                    return htmlspecialchars($value, ENT_COMPAT, static::CHARSET);
-                }
-            ]);
-    }
+    protected static $mustacheParams = null;
 
     /**
      *
@@ -81,19 +52,46 @@ class Template
      */
     public static function getInstance()
     {
-        if (!static::$instance) {
-            static::$instance = new Template();
+        if (null == static::$mustacheRender) {
+            $templatesFolder = static::getTemplatesFolderLocation();
+            static::$mustacheRender = new Mustache_Engine(
+                [
+                    'charset' => static::CHARSET,
+                    'strict_callables' => static::STRICT_CALLABLES,
+                    'cache_file_mode' => static::CACHE_FILE_MODE,
+                    'cache_lambda_templates' => static::CACHE_LAMBDA_TEMPLATES,
+                    'loader' => new Mustache_Loader_FilesystemLoader($templatesFolder),
+                    'partials_loader' => new Mustache_Loader_FilesystemLoader($templatesFolder),
+                    'logger' => new Mustache_Logger_StreamLogger('php://stderr'),
+                    'helpers' => static::getHelpers(),
+                    'pragmas' => static::getPragmas(),
+                    'escape' => function ($value)
+                    {
+                        return htmlspecialchars($value, ENT_COMPAT, static::CHARSET);
+                    }
+                ]);
         }
-        return static::$instance;
+        return static::$mustacheRender;
     }
 
     /**
+     * Return all params
      *
-     * @return \Mustache_Engine
+     * @return array<string,object>
      */
-    public function getRenderEngine()
+    public static function getMustacheParams()
     {
-        return $this->renderEngine;
+        if (null == static::$mustacheParams) {
+
+            $baseParamsFile = VENDOR_KNOB_BASE_DIR . '/src/config/' . static::$mustacheParamsFile . '.php';
+            $appParamsFile = APP_DIR . '/config/' . static::$mustacheParamsFile . '.php';
+
+            $baseParams = (file_exists($baseParamsFile)) ? require $baseParamsFile : [];
+            $appParams = (file_exists($appParamsFile)) ? require $appParamsFile : [];
+
+            static::$mustacheParams = array_merge($baseParams, $appParams);
+        }
+        return static::$mustacheParams;
     }
 
     /**
@@ -110,7 +108,7 @@ class Template
      *
      * @return multitype:string
      */
-    protected function getPragmas()
+    protected static function getPragmas()
     {
         return [
             Mustache_Engine::PRAGMA_FILTERS,
@@ -123,7 +121,7 @@ class Template
      *
      * @return array<function>
      */
-    protected function getHelpers()
+    protected static function getHelpers()
     {
         if (file_exists($pathHelpers = APP_DIR . '/config/' . static::$mustacheHelpersFile . '.php')) {
             $mustacheHelpersFile = include $pathHelpers;
